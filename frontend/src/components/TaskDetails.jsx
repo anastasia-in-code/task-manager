7ec478d81d9faf85a48de371da9565b3d3ae3c4b
Anastasiia <anastasiia.perih@gmail.com>
2024-03-18 14:07:25 -0400

    FE: task delete functionality

--- frontend/src/components/TaskDetails.jsx
+++ frontend/src/components/TaskDetails.jsx
@@ -3,14 +3,54 @@ import Card from "react-bootstrap/Card";
 
 export const TaskDetails = ({ tasks, selectedTask }) => {
   const taskData = tasks.find((task) => task.id === selectedTask);
+
+  const handleTaskDelete =() => {
+    fetch(`http://127.0.0.1:5000/tasks/${taskData.id}`, {
+        method: "DELETE",
+      })
+        .then((response) => {
+          if (response.ok) {
+            console.log(response.body)
+            console.log('deleted')
+          } else {
+            console.error("Error deleting task:", response.statusText);
+          }
+        })
+        .catch((error) => {
+          console.error("Error deleting task:", error);
+        });
+  }
+  
   return (
     <Card style={{ width: "100%" }}>
       {taskData && (
         <Card.Body>
-          <Card.Title style={{ fontSize: "1.5rem" }}>{taskData.title}</Card.Title>
+          <Card.Title
+            className="d-flex justify-content-between align-items-center"
+            style={{ fontSize: "1.5rem" }}
+          >
+            <span>{taskData.title}</span>
+            <a onClick={handleTaskDelete} className="cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                fill="currentColor"
+                className="bi bi-trash3 mx-5"
+                viewBox="0 0 16 16"
+              >
+                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+              </svg>
+            </a>
+          </Card.Title>
           <Card.Text>
             Status:{" "}
-            <span style={{ color: taskData.completed ? "green" : "red", fontSize: "1.2rem" }}>
+            <span
+              style={{
+                color: taskData.completed ? "green" : "red",
+                fontSize: "1.2rem",
+              }}
+            >
               {taskData.completed ? "completed" : "active"}
             </span>
           </Card.Text>
