53a9822158652ed6d836472cf51e729227b15056
Anastasiia <anastasiia.perih@gmail.com>
2024-03-18 20:26:55 -0400

    FE: icon anchors converted into buttons

--- frontend/src/components/TaskDetails.jsx
+++ frontend/src/components/TaskDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import Card from "react-bootstrap/Card";
 import EditTaskModal from "./EditTaskModal";
 import {
@@ -9,24 +9,29 @@ import {
 import TasksAPI from "../api/TasksAPI";
 
 export const TaskDetails = () => {
+  const dispatch = useTasksDispatch();
   const [showEditTaskForm, setShowEditTaskForm] = useState(false);
   const { tasks, selectedTask } = useTasksState();
-  const dispatch = useTasksDispatch();
-  const taskData = tasks.find((task) => task.id === selectedTask);
+
+  const taskData = useMemo(() => tasks.find((task) => task.id === selectedTask), [tasks, selectedTask]);
 
   const handleTaskEdit = () => setShowEditTaskForm(true);
 
   const handleTaskDelete = async () => {
-    await TasksAPI.deleteTask(taskData.id)
-    dispatch({
+    try {
+      await TasksAPI.deleteTask(taskData.id);
+      dispatch({
         type: StateActions.DELETE_TASK,
-        payload: taskData.id
-      })
+        payload: taskData.id,
+      });
+    } catch (error) {
+      console.log(error)
+    }
   };
 
   return (
     <>
-      {taskData && (
+      {showEditTaskForm && (
         <EditTaskModal
           show={showEditTaskForm}
           task={taskData}
@@ -43,7 +48,7 @@ export const TaskDetails = () => {
             >
               <span>{taskData.title}</span>
               <div>
-                <a onClick={handleTaskEdit} className="cursor-pointer">
+                <button onClick={handleTaskEdit} className="icon-btn" aria-label="Edit Task">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="25"
@@ -54,8 +59,8 @@ export const TaskDetails = () => {
                   >
                     <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z" />
                   </svg>
-                </a>
-                <a onClick={handleTaskDelete} className="cursor-pointer">
+                </button>
+                <button onClick={handleTaskDelete} className="icon-btn" aria-label="Delete Task">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="25"
@@ -66,7 +71,7 @@ export const TaskDetails = () => {
                   >
                     <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
                   </svg>
-                </a>
+                </button>
               </div>
             </Card.Title>
             <Card.Text>
